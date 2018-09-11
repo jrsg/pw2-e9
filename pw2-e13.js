1884f0b27e6fe9913b29d9232515796e0d1a4944
Ruben Sanchez <jrsg@outlook.com>
2018-09-11 16:45:51 -0500

    Carga inicial

---
+++ pw2-e13.js
@@ -0,0 +1,12 @@
+ var listaEstados = ['Aguascalientes', 'Baja California', 'Baja California Sur', 
+'Campeche', 'Coahuila', 'Colima', 'Chiapas', 'Chihuahua', 'Distrito Federal', 'Durango', 'Guanajuato ',
+'Guerrero', 'Hidalgo', 'Jalisco', 'Michoacán', 'Estado de México', 'Morelos', 'Nayarit', 'Nuevo León', 
+'Oaxaca', 'Puebla', 'Quintana Roo', 'Querétaro', 'San Luis Potosí', 'Sinaloa', 'Sonora', 'Tabasco', 'Tlaxcala', 
+'Tamaulipas', 'Veracruz', 'Yucatán', 'Zacatecas'];
+
+var usuariosRegistrados = ['amorales', 'cfuentes', 'dcarrillo', 'elopez', 'fdominguez', 'hmarquez','mzamudio', 'pcontreras','rsanchez','shernandez'];        
+       
+$(document).ready(function(){
+    
+      
+});
